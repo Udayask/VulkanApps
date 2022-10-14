21e354db3a80b3f680dc14cf2203bb9b8dd389ea
Udayask <udaya.s.k@intel.com>
2022-10-14 22:09:21 +0530

    Add base simpletriangle app

---
+++ SimpleTriangle/main.cpp
@@ -0,0 +1,15 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+#include <vulkan/vulkan.h>
+
+
+#include <iostream>
+
+
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello" << std::endl;
+    return 0;
+}
